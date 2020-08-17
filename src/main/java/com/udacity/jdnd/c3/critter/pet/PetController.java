63bba8409593122fcbb0b19528ea4c367c6514b8
Neil Majumdar <neil.majumdar19@gmail.com>
2020-08-16 22:43:18 -0700

    added Pet Data Access and Service Layers

--- src/main/java/com/udacity/jdnd/c3/critter/pet/PetController.java
+++ src/main/java/com/udacity/jdnd/c3/critter/pet/PetController.java
@@ -1,30 +1,62 @@
 package com.udacity.jdnd.c3.critter.pet;
 
+import com.udacity.jdnd.c3.critter.entity.Pet;
+import com.udacity.jdnd.c3.critter.service.PetService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/pet")
 public class PetController {
 
+    @Autowired
+    private PetService petService;
+
     @PostMapping
     public PetDTO savePet(@RequestBody PetDTO petDTO) {
-        throw new UnsupportedOperationException();
+        petService.savePet(convertPetDTOToPet(petDTO));
+        return petDTO;
     }
 
     @GetMapping("/{petId}")
     public PetDTO getPet(@PathVariable long petId) {
-        throw new UnsupportedOperationException();
+        return convertPetToPetDTO(petService.getPetById(petId));
+
     }
 
     @GetMapping
     public List<PetDTO> getPets(){
-        throw new UnsupportedOperationException();
+        return convertPetsToPetDTOs(petService.getPets());
     }
 
     @GetMapping("/owner/{ownerId}")
     public List<PetDTO> getPetsByOwner(@PathVariable long ownerId) {
-        throw new UnsupportedOperationException();
+        return convertPetsToPetDTOs(petService.getPetsByOwner(ownerId));
+    }
+
+    private static Pet convertPetDTOToPet(PetDTO petDTO)
+    {
+         Pet pet = new Pet();
+         BeanUtils.copyProperties(petDTO, pet);
+         return pet;
+    }
+
+    private static PetDTO convertPetToPetDTO(Pet pet)
+    {
+        PetDTO petDTO = new PetDTO();
+        BeanUtils.copyProperties(pet, petDTO);
+        return petDTO;
+    }
+
+    private static List<PetDTO> convertPetsToPetDTOs(List<Pet> pets)
+    {
+        List<PetDTO> petDTOs = new ArrayList<PetDTO>();
+        for(Pet p : pets)
+            petDTOs.add(convertPetToPetDTO(p));
+        return petDTOs;
     }
 }
---
+++ src/main/java/com/udacity/jdnd/c3/critter/service/PetService.java
@@ -0,0 +1,37 @@
+package com.udacity.jdnd.c3.critter.service;
+
+import com.udacity.jdnd.c3.critter.entity.Pet;
+import com.udacity.jdnd.c3.critter.repository.PetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class PetService {
+
+    @Autowired
+    private PetRepository petRepository;
+
+    public Pet savePet(Pet pet)
+    {
+        return petRepository.save(pet);
+    }
+
+    public Pet getPetById(Long petId)
+    {
+        return petRepository.findById(petId).get();
+    }
+
+    public List<Pet> getPets()
+    {
+        return petRepository.findAll();
+    }
+
+    public List<Pet> getPetsByOwner(Long ownerId)
+    {
+        return petRepository.findByOwnerId(ownerId);
+    }
+}
